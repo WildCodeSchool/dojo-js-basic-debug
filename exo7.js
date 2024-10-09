2df9583ce3549fa11d192893cbb0eba3cf7dd79c
Fantine <fantine.rudent@hotmail.fr>
2024-10-09 14:50:22 +0200

    new additions exo7,8,9

---
+++ exo7.js
@@ -0,0 +1,6 @@
+let myArray = [10, 20, 30];
+myArray.pop();
+console.log(myArray[2]);
+
+// que me renvoie le console.log & pourquoi ?
+
---
+++ exo8.js
@@ -0,0 +1,6 @@
+let age = 18;
+if ((age = 17)) {
+	console.log("You are an adult");
+}
+
+// qu'est ce qui va s'afficher et pourquoi ?;
